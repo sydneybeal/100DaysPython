6fbd2bde39eac7df9b56f9eace9a8578a1a98827
sydneybeal <sydneybeal@miami.edu>
2019-06-10 20:49:34 -0400

    day 9

--- Module1/Day09/module1_day09_slicing.py
+++ Module1/Day09/module1_day09_slicing.py
@@ -6,3 +6,12 @@
     Description:    <REPLACE>
 """
 
+quotes = ["Pitter patter, let's get at 'er", "Hard no!", "H'are ya now?", "Good-n-you?", "Not so bad.", "Is that what you appreciates about me?"]
+quotes[0]
+print(f"{quotes[2]}\n\t{quotes[3]}\n{quotes[4]}")
+
+print(quotes[2:5])
+
+print(quotes[::5])
+
+print(quotes[::-1])
